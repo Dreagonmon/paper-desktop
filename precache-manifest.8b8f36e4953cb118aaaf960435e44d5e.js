a49f64fef68f292d3058b0d19b2c8278685d047d
Dreagonmon <531486058@qq.com>
2020-06-18 15:41:47 +0800

    gh-pages branch init

---
+++ precache-manifest.8b8f36e4953cb118aaaf960435e44d5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7470c797a7194c378df2943d1f65e7",
+    "url": "/paper-desktop/index.html"
+  },
+  {
+    "revision": "73961a5fbbc0880c403c",
+    "url": "/paper-desktop/js/app.1bd82e30.js"
+  },
+  {
+    "revision": "8f10d8c7852edf664655",
+    "url": "/paper-desktop/js/chunk-vendors.4a3fdea9.js"
+  },
+  {
+    "revision": "a927adfb3481caf425c14c76c92fce9b",
+    "url": "/paper-desktop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/paper-desktop/robots.txt"
+  }
+]);
